a815acd9f6bd86fc3e41aeff44a686dc86c8459c
Effectivement <rnssaketo@proton.me>
2026-07-29 23:17:14 -0400

    i forgot to change the url to nix4nvchad

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix4nvchad = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/nix4nvchad";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
