0c5414e9b514361a180f6199105b571c2a1f159d
Effectivement <rnssaketo@proton.me>
2026-07-30 21:03:46 -0400

    use zen kernel

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   networking.hostName = "nixfx"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
