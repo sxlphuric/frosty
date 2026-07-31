21fd04e192e497c9207d2aff73eab09b0c8e7b35
Effectivement <rnssaketo@proton.me>
2026-07-30 21:21:00 -0400

    allow passwordless running of nixos-rebuild for mushroom

--- configuration.nix
+++ configuration.nix
@@ -163,5 +163,20 @@
   security.sudo.enable = false;
   security.sudo-rs = {
     enable = true;
+    extraRules = [
+      {
+        users = [ "mushroom" ];
+        commands = [
+          {
+            command = "/run/current-system/sw/bin/nixos-rebuild";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "/etc/profiles/per-user/mushroom/bin/nixos-rebuild";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
   };
 }
