471503e2af9c259db474c98b57379a34618ffcd3
Effectivement <rnssaketo@proton.me>
2026-08-05 16:27:33 -0400

    i put the wrong path

--- user/programs.nix
+++ user/programs.nix
@@ -3,16 +3,16 @@
     inputs.nix4nvchad.homeManagerModules.default
     inputs.zen-browser.homeModules.twilight
 
-    ./user/programs/starship.nix
-    ./user/programs/shells.nix
-    ./user/programs/kitty.nix
-    ./user/programs/zed-editor.nix
-    ./user/programs/zen-browser.nix
-    ./user/programs/git.nix
-    ./user/programs/cava.nix
-    ./user/programs/fastfetch.nix
-    ./user/programs/obsidian.nix
-    ./user/programs/nvchad.nix
+    ./programs/starship.nix
+    ./programs/shells.nix
+    ./programs/kitty.nix
+    ./programs/zed-editor.nix
+    ./programs/zen-browser.nix
+    ./programs/git.nix
+    ./programs/cava.nix
+    ./programs/fastfetch.nix
+    ./programs/obsidian.nix
+    ./programs/nvchad.nix
   ];
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
