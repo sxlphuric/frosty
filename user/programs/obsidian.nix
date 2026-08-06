ecff1bed3aab0b126211e2202348b64cc2727707
Effectivement <rnssaketo@proton.me>
2026-08-06 05:16:34 -0400

    add obsidian vaults and core plugins

--- user/programs/obsidian.nix
+++ user/programs/obsidian.nix
@@ -1,6 +1,50 @@
 {
-  # TODO config this (transfer the config)
   programs.obsidian = {
     enable = true;
+
+    vaults = {
+      "Obsidian" = {
+        enable = true;
+        target = "Documents/Obsidian";
+      };
+    };
+
+    defaultSettings = {
+      app = {
+        alwaysUpdateLinks = true;
+      };
+      corePlugins = [
+        "file-explorer"
+        "global-search"
+        "graph"
+        "backlink"
+        "canvas"
+        "outgoing-link"
+        "tag-pane"
+        # "footnotes"
+        "properties"
+        "page-preview"
+        # "daily-notes"
+        "templates"
+        "note-composer"
+        "command-palette"
+        "slash-command"
+        "editor-status"
+        "bookmarks"
+        # "markdown-importer"
+        # "zk-prefixer"
+        # "random-note"
+        "outline"
+        "word-count"
+        # "slides"
+        # "audio-recorder"
+        # "workspaces"
+        "file-recovery"
+        # "publish"
+        # "sync"
+        "bases"
+        "webviewer"
+      ];
+    };
   };
 }
