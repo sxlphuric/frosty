5c06e1c0887727bf461ecf16f92d2f5187cb41e5
Effectivement <rnssaketo@proton.me>
2026-08-06 02:16:35 -0400

    switch to grub + extra bootloader entries for system

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,7 +1,32 @@
 {pkgs, ...}: {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      useOSProber = false;
+
+      extraEntriesBeforeNixOS = true;
+      extraEntries = let
+        createLinuxGrubEntry = { name, path }: ''
+          menuentry "${name}" {
+            insmod part_gpt
+            insmod fat
+
+            search --no-floppy --set=root <ESP-FS-UUID>
+            chainloader ${path}/grubx64.efi
+          }
+        '';
+      in ''
+        ${createLinuxGrubEntry { name = "OpenSUSE Tumbleweed Bootloader"; path = "/EFI/opensuse"; }}
+        ${createLinuxGrubEntry { name = "EndeavourOS Bootloader"; path = "/EFI/endeavouros"; }}
+      '';
+    };
+  };
 
   # Use latest ZEN kernel.
   boot.kernelPackages = pkgs.linuxPackages_zen;
