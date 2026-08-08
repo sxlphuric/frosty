6bee6200ebcff8fbf79e7ecb2026cf9452a0874e
Effectivement <rnssaketo@proton.me>
2026-08-08 19:07:28 -0400

    add fish functions for nix shell and nix run

--- user/programs/shells.nix
+++ user/programs/shells.nix
@@ -5,6 +5,17 @@
       set fish_greeting
       fastfetch
     '';
+    functions = {
+      run = "nix run nixpkgs#$argv[1] -- $argv[2..-1]";
+      shell = ''
+        set NIX_SHELL_CMD "nix shell"
+        for i in $argv
+          set NIX_SHELL_CMD "$NIX_SHELL_CMD nixpkgs#$i"
+        end
+        eval $NIX_SHELL_CMD
+        set -e NIX_SHELL_CMD
+      '';
+    };
   };
   programs.zsh.enable = true;
 }
