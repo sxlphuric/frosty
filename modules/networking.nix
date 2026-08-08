7ec60ffda7abcaa6e43719623d99597fac5fdc46
Effectivement <rnssaketo@proton.me>
2026-08-08 03:51:24 -0400

    nuke wifi details

--- modules/networking.nix
+++ modules/networking.nix
@@ -52,28 +52,7 @@
               route-metric = 100;
             };
           };
-          "SSID" = {
-            connection = {
-              id = "wlo1-ssid";
-              type = "wifi";
-              interface-name = "wlo1";
-              autoconnect = "true";
-            };
-            wifi = {
-              ssid = "SSID";
-              mode = "infrastructure";
-            };
-            wifi-security = {
-              key-mgmt = "wpa-psk";
-              psk = "PSK";
-            };
-            ipv4 = {
-              method = "manual";
-              address1 = "192.168.2.188/24,192.168.2.1";
-              dns = "192.168.2.190;1.1.1.1;1.0.0.1";
-            };
-          };
-        };
+                  };
       };
     };
   };
