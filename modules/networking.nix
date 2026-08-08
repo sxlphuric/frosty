09845ee08f8d40e63c4b5769d9469892bdb663a8
Effectivement <rnssaketo@proton.me>
2026-08-08 02:38:33 -0400

    before nuking this im commiting this because im really proud of this thing i made (which doesnt even work)

--- modules/networking.nix
+++ modules/networking.nix
@@ -14,10 +14,23 @@
               interface-name = "eno1";
               autoconnect = true;
             };
-            ipv4 = {
+            ipv4 = let routes = [
+              { address = "192.168.10.0"; prefixLength = 28; via = "192.168.10.1"; }
+              { address = "192.168.20.0"; prefixLength = 27; via = "192.168.10.1"; }
+              { address = "192.168.30.0"; prefixLength = 30; via = "192.168.10.1"; }
+              { address = "192.168.40.0"; prefixLength = 27; via = "192.168.10.1"; }
+              { address = "192.168.50.0"; prefixLength = 30; via = "192.168.10.1"; }
+            ];
+
+            createNMRoute = { address, prefixLength, via }: "${address}/${toString prefixLength} ${via}";
+            createNMRoutes = routes: "${builtins.concatStringsSep ";" (map createNMRoute routes)};";
+
+
+            in {
               method = "manual";
               address1 = "192.168.10.11/28,192.168.10.1";
               dns = "192.168.10.1;1.1.1.1;1.0.0.1;";
+              routes = ["192.168.10.0/28 192.168.10.1"];
             };
           };
           "SSID" = {
