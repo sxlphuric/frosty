92c5c47ec48b4848fcdbfeda8c79bb6c2d46d233
Effectivement <rnssaketo@proton.me>
2026-08-12 19:04:58 -0400

    replace ssh key in secrets.nix with the publickey that has been generated after having added my hostname

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   mushroom-nixfx = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7ExyVU1Cg47jCjSy/YdanXqitFSb+e462m6yBWRT2R mushroom@nixfx";
-  mushroom-mushroom-machine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/db7MER7cs7Ic6Frkn9BAgmzCed2CSHlXmvzlvFjpj mushroom@nixfx";
+  mushroom-mushroom-machine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDZhiA6zP7O/C+Zhis4QYRBZ2l9Ig7MQ0900G1CnelAL mushroom@mushroom-machine";
   users = [mushroom-nixfx mushroom-mushroom-machine];
 in {
   "syncthingPassword.age".publicKeys = users;
