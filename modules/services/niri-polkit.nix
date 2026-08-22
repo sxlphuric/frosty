540fce59dbca2df0149230f40f5a1510a27c60d3
Effectivement <rnssaketo@proton.me>
2026-08-22 10:23:27 -0400

    format: alejandra

--- modules/services/niri-polkit.nix
+++ modules/services/niri-polkit.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   systemd.user.services.polkit-mate-authentication-agent-1 = {
     description = "polkit-mate-authentication-agent-1";
-    wantedBy = [ "niri.service" ];
-    wants = [ "niri.service" ];
-    after = [ "niri.service" ];
+    wantedBy = ["niri.service"];
+    wants = ["niri.service"];
+    after = ["niri.service"];
     serviceConfig = {
       Type = "simple";
       ExecStart = "${pkgs.mate-polkit}/libexec/polkit-mate-authentication-agent-1";
