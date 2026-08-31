ed52472d5e806d1de6ec7458b3e0f3b47bf05d65
Effectivement <rnssaketo@proton.me>
2026-08-30 20:07:28 -0400

    add chromeos plymouth theme and silent boot

--- mushroom-machine/modules/boot.nix
+++ mushroom-machine/modules/boot.nix
@@ -1,13 +1,57 @@
-{pkgs, ...}: {
-  boot.loader = {
-    efi.canTouchEfiVariables = false;
-    timeout = 5;
-    grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
-      useOSProber = false;
-      efiInstallAsRemovable = true;
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  plymouth-theme-chromeos = pkgs.stdenvNoCC.mkDerivation {
+    pname = "plymouth-theme-chromeos";
+    version = "acf6d2f";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "e9x";
+      repo = "plymouth-theme-chromeos";
+      rev = "acf6d2fcb1b89aeb34d018a4817642dee9c278c5";
+      hash = "sha256-7MN5/T8SDzusg+JvP59up+Vbq7/AjoR1dRdxNibXHUM=";
     };
+
+    postPatch = ''
+      # Remove not needed files
+      rm README.md LICENSE
+    '';
+
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/share/plymouth/themes/chromeos
+      cp chromeos/* $out/share/plymouth/themes/chromeos
+      find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@\/usr\/@$out\/@" {} \;
+      runHook postInstall
+    '';
+  };
+in {
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = false;
+      timeout = 0;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = false;
+        efiInstallAsRemovable = true;
+      };
+    };
+    plymouth = {
+      theme = "chromeos";
+      themePackages = [plymouth-theme-chromeos];
+    };
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
+    ];
   };
 }
